0e3c573550fd55a96113adeabcaf9db8181d8d88
Jaci <jaci.brunning@gmail.com>
2019-07-27 21:47:33 +0800

    Update to new format for xauth containers

--- melodic/ros_entrypoint.sh
+++ melodic/ros_entrypoint.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 set -e
 
+# setup user
+if [[ -n "$UID" ]]; then
+  useradd -m dev
+  echo "dev:dev" | chpasswd
+  usermod --shell /bin/bash dev
+  usermod -aG sudo dev
+  echo "dev ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/dev
+  chmod 0440 /etc/sudoers.d/dev
+  usermod --uid $UID dev
+  usermod --gid $GID dev
+
+  # switch into new user
+  exec su -- dev
+  . ~/.profile
+fi
+
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 exec "$@"
--- ros-docker.sh
+++ ros-docker.sh
@@ -2,28 +2,52 @@
 
 ROS_DOCKER_IMAGE=${ROS_DOCKER_IMAGE:-jaci/ros}
 ROS_DOCKER_HOME=${ROS_DOCKER_HOME:-$HOME}
-ROS_USE_NVIDIA=true
+ROS_DOCKER_UNCONFINED=${ROS_DOCKER_UNCONFINED:-true}
+ROS_DOCKER_NVIDIA=${ROS_DOCKER_NVIDIA:-true}
 
-# TODO: User remap on linux
+ROS_DOCKER_XSOCK=/tmp/.X11-unix
+ROS_DOCKER_XAUTH=/tmp/.docker.xauth
 
-ROS_DOCKER_ARGS=(
-  --env="DISPLAY"
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw"
-  # --volume="/etc/group:/etc/group:ro"
-  # --volume="/etc/passwd:/etc/passwd:ro"
-  # --volume="/etc/shadow:/etc/shadow:ro"
-  # --volume="/etc/sudoers.d:/etc/sudoers.d:ro"
-  # --user=$(id -u)
-  --env HOME=$HOME
-  --privileged
-  --net=host
-)
+ros-xauth() {
+  touch $XAUTH
+  xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+}
 
 ros-launch() {
-  local args=( "${ROS_DOCKER_ARGS[@]}" --volume="$ROS_DOCKER_HOME:$HOME" --volume="$(pwd):/work" )
-  if [ "$ROS_USE_NVIDIA" == "true" ]; then
+  local args=( 
+    --env="DISPLAY" 
+    --env HOME=$HOME 
+    --env UID=$UID
+    --env GID=$GID
+    --net=host 
+    --volume="$ROS_DOCKER_HOME:$HOME" 
+    --volume="$(pwd):/work"
+  )
+
+  # XOrg
+  local XSOCK=$ROS_DOCKER_XSOCK
+  local XAUTH=$ROS_DOCKER_XAUTH
+  args=( 
+    "${args[@]}" 
+    --volume="${XSOCK}:${XSOCK}:rw" 
+    --volume="${XAUTH}:${XAUTH}:rw" 
+    --env="XAUTHORITY=${XAUTH}"
+  )
+
+  # NVIDIA
+  if [[ "$ROS_DOCKER_NVIDIA" == "true" ]]; then
     args=( "${args[@]}" --runtime=nvidia )
   fi
+
+  # Security
+  if [[ "$ROS_DOCKER_UNCONFINED" == "true" ]]; then
+    # Required for melodic (ubuntu 18.04 container).
+    # Really, it boils down to this: https://github.com/moby/moby/issues/38442
+    # To avoid having to add new apparmor profiles, we can run unconfined. The ROS installation
+    # should be fairly trusted, but it can be avoided by setting ROS_DOCKER_UNCONFINED to false
+    args=( "${args[@]}" --security-opt apparmor:unconfined )
+  fi
+
   docker run ${args[@]} $@
 }
 
